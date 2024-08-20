11f2e67f0a53da012e6ff49ac98c5e05ab5e90e3
smanna123 <sayantanmanna840@gmail.com>
2024-08-20 16:09:23 +0530

    commit

--- inference.py
+++ inference.py
@@ -1,17 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-DIR = "nlbb"
+DIR = "nlbb_distilled"
 model = AutoModelForSeq2SeqLM.from_pretrained(DIR)
 tokenizer = AutoTokenizer.from_pretrained(DIR)
 #model.to_bettertransformer()
 model.to('cuda')
 text = "he is a good boy"
 
-inputs = tokenizer(text, return_tensors="pt")
+inputs = tokenizer(text, return_tensors="pt").to('cuda')
 
 translated_tokens = model.generate(
     **inputs, forced_bos_token_id=tokenizer.convert_tokens_to_ids("pan_Guru"), max_length=512
 )
-# print(tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0])
+print(tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0])
 
 
 # import torch
